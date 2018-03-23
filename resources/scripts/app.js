343eea3b327e8796d8379d99a081fa8e05559fa0
antonioSF <utlizador@utlizadors-MBP.home>
2018-03-23 13:01:37 +0000

    final version

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -11,10 +11,10 @@ const fadeInScene = () => {
   })
 
   initScene.then(setTimeout(() => {
-    	if (animWrapper.classList.contains('is--hidden')) {
-    		animWrapper.classList.remove('is--hidden')
-       	 	animWrapper.classList.add('is--visible')
-    	}
+    if (animWrapper.classList.contains('is--hidden')) {
+      animWrapper.classList.remove('is--hidden')
+      animWrapper.classList.add('is--visible')
+    }
   }, 1000))
 }
 
